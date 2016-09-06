a5e69d438def2d8e68980ada438990b96f9a05c8
kaiz <kaiz.hudda@gmail.com>
2016-09-06 15:56:13 +1200

    added base files worked on before creating github account

---
+++ js/prac.js
@@ -0,0 +1,17 @@
+function loadDoc(form) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("placeData").innerHTML = "";
+            var dataParse = xhttp.responseText;
+            var obj = JSON.parse(dataParse);
+            for (var i = 0; i < 5; i++) {
+                document.getElementById("placeData").innerHTML += obj.noun.syn[i] + '<br />';
+            }
+            console.log(obj);
+        }
+    };
+    var x = document.getElementById("myForm").value;
+    xhttp.open("GET", "http://words.bighugelabs.com/api/2/1b1981d0d07796d1f0f57b643e1cee62/" + x + "/json");
+    xhttp.send();
+}
